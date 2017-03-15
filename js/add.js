dc095d3b8fd38dc204737f1094ad23de399621fa
SHIYAFEIS <756983215@qq.com>
2017-03-15 14:34:34 +0800

    modification

--- js/add.js
+++ js/add.js
@@ -8,4 +8,10 @@ $(function(){
     $(".jv a").on("click","span",function(){
         $(this).parent().remove();
     })
+    $(".jia").on("click",function(){
+        $(this).prev().clone().insertBefore($(this));
+        $(".jv a").on("click","span",function(){
+            $(this).parent().remove();
+        })
+    })
 })
